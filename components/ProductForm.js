cb5979bebb859bbb777b70f897e6039ad6ace936
BRITHE <bthellier51@gmail.com>
2022-01-20 18:53:21 +0100

    final steps

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -56,7 +56,7 @@ export default function ProductForm({ product }) {
 	return (
 		<div className="rounded-2xl p-4 shadow-lg flex flex-col w-full md:w-1/3">
 			<h2 className="text-2xl font-bold">{product.title}</h2>
-			<span className="pb-6">
+			<span className="pb-3">
 				{formatter.format(product.variants.edges[0].node.priceV2.amount)}
 			</span>
 			{product.options.map(({ name, values }) => (
@@ -70,7 +70,7 @@ export default function ProductForm({ product }) {
 			))}
 			<button
 				onClick={() => addToCart(selectedVariant)}
-				className="bg-black rounded-lg text-white px-2 py-3 hover:bg-gray-800"
+				className="bg-black rounded-lg text-white px-2 py-3 mt-3 hover:bg-gray-800"
 			>
 				Add To Cart
 			</button>
